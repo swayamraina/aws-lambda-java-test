29f1bcea11ee5ebca3b1f7c88f5d72503d10ee95
swayamraina <swayamraina@gmail.com>
2020-05-21 17:06:02 +0530

    added response object

---
+++ src/main/java/response/Response.java
@@ -0,0 +1,13 @@
+package response;
+
+public class Response {
+
+    public Float amount;
+    public String version;
+
+
+    public Response () {}
+
+    public Response (Float amount, String version) { this.amount=amount; this.version=version; }
+
+}
